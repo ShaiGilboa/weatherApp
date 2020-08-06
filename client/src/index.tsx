818997bf9e889fdf0025a5aca47af1943d850026
Shai Gilboa <shizel@gmail.com>
2020-08-05 20:04:23 -0400

    serviceworker setup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import configureStore from './Redux/Store';
 import { Provider } from 'react-redux';
+import * as serviceWorker from '../PWA/serviceWorker';
 
 const reduxStore = configureStore();
 
@@ -15,3 +16,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
